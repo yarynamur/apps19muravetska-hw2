47075967125c4ed250af300d3bf780a95574ebe7
yarynamur <45094036+yarynamur@users.noreply.github.com>
2019-11-03 19:45:10 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,28 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    public ImmutableLinkedList queue;
+
+    public Queue(){
+        queue = new ImmutableLinkedList();
+    }
+
+    public Object peek() {
+        return queue.getFirst();
+    }
+
+    public Object dequeue() {
+        if (queue.len == 0){
+            throw new IndexOutOfBoundsException();
+        }
+        Object el = queue.getFirst();
+        queue = queue.removeFirst();
+        return el;
+    }
+
+    public void enqueue(Object e) {
+        queue = queue.addLast(e);
+    }
 }
