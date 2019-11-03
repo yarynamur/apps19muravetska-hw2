7cb6d7d12998fe6bccfbe02e6e8c9083dcd7f76f
yarynamur <45094036+yarynamur@users.noreply.github.com>
2019-11-03 19:45:33 +0200

    Update Stack.java

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,30 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    public ImmutableLinkedList stack;
+
+    public Stack() {
+        stack = new ImmutableLinkedList();
+    }
+
+    Object peek() {
+        if (stack.len == 0){
+            throw new IndexOutOfBoundsException();
+        }
+        return stack.getFirst();
+    }
+
+    Object pop() {
+        Object e = stack.getFirst();
+        stack = stack.removeFirst();
+        return e;
+    }
+
+    void push(Object e) {
+        stack = stack.addFirst(e);
+    }
+
+
 }
